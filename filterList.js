2ad51fb06930a982d106cf807adb7bc8189f7244
Vanger888 <sergh.vanger@gmail.com>
2019-02-15 16:29:12 +0200

    filterList.js, maskify.js

---
+++ filterList.js
@@ -0,0 +1,9 @@
+/*In this kata you will create a function that
+ takes a list of non-negative integers and strings 
+ and returns a new list with the strings filtered out.
+*/
+function filter_list(l) {
+  return l.filter((item) => Number.isInteger(item));
+}
+
+console.log(filter_list([1,2,'a','b']));
